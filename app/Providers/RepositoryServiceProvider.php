3199eafc9579e1dfbbfe7195805c690d3df00e63
Mateus Azevedo <mateusazevedo@gmail.com>
2016-07-05 01:19:44 +0000

    Refatorado service provider para extrair binds por resource

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -23,11 +23,27 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerClientRepository();
+
+        $this->registerProjectRepository();
+    }
+
+    /**
+     * Register repositories for Client resource.
+     */
+    private function registerClientRepository()
+    {
         $this->app->bind(
             'CursoLaravel\Repositories\Client\ClientRepository',
             'CursoLaravel\Repositories\Client\ClientRepositoryEloquent'
         );
+    }
 
+    /**
+     * Register repositories for Project resource.
+     */
+    private function registerProjectRepository()
+    {
         $this->app->bind(
             'CursoLaravel\Repositories\Project\ProjectRepository',
             'CursoLaravel\Repositories\Project\ProjectRepositoryEloquent'
