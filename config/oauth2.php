aa9a5e49c4be2f5a25109f972782ce79a0b7b050
Mateus Azevedo <mateusazevedo@gmail.com>
2016-07-08 02:44:36 +0000

    Configurando refresh token

--- config/oauth2.php
+++ config/oauth2.php
@@ -33,6 +33,11 @@ return [
             'callback' => '\CursoLaravel\OAuth\CredentialsVerifier@verify',
             'access_token_ttl' => 3600,
         ],
+        'refresh_token' => [
+            'class' => \League\OAuth2\Server\Grant\RefreshTokenGrant::class,
+            'access_token_ttl' => 3600,
+            'refresh_token_ttl' => 36000,
+        ],
     ],
 
     /*
